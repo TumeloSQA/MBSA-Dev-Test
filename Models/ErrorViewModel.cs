d3e095a82b95f7f457c15a26f8f85f548753f025
Tumelo Maremane <Tumelo.Maremane@ssp-worldwide.com>
2019-06-12 00:13:10 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MBSA_Dev_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
