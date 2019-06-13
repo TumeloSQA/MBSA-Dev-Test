4ab42de86b165b085694c1044cebc9ef5028eb0d
Tumelo Maremane <tumelophuti@gmail.com>
2019-06-13 15:44:40 +0200

    Get Products Page

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,30 +1,35 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using MBSA_Dev_Test.Models;
-//using MBSA_Dev_Test.DbContexts;
-//using MBSA_Dev_Test.DAL;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MBSA_Dev_Test.Models;
+using MBSA_Dev_Test.DbContexts;
+using MBSA_Dev_Test.DAL;
 
-//namespace MBSA_Dev_Test.Controllers
-//{
-//    public class ProjectsController : Controller
-//    {
-//        private readonly MBSADbContext _context;
+namespace MBSA_Dev_Test.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly MBSADbContext _context;
 
-//        private readonly IProjectsRepository _projectsRepository;
-//        public ProjectsController(IProjectsRepository projectsRepository, MBSADbContext dbContext)
-//        {
-//            _projectsRepository = projectsRepository;
-//            _context = dbContext;
-//        }
+        private readonly IProjectsRepository _projectsRepository;
+        public ProjectsController(IProjectsRepository projectsRepository, MBSADbContext dbContext)
+        {
+            _projectsRepository = projectsRepository;
+            _context = dbContext;
+        }
 
-//        public async Task<JsonResult> GetProjects()
-//        {
-//            return View(await _projectsRepository.GetProjects().ToList());
+        //public async Task<JsonResult> GetProjects()
+        //{
+        //    return Json(await _projectsRepository.GetProjects().ToListAsync());
+        //}
 
-//        }
+        public IEnumerable<Project> GetProject()
+        {
+            return  _projectsRepository.GetProjects().ToList();
+        }
 
-//    }
-//}
+    }
+
+}
