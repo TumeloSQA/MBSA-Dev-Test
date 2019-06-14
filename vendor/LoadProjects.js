f3d1acd80d86933d141fbe95aa9b4316e471a79c
Tumelo Maremane <tumelophuti@gmail.com>
2019-06-14 14:56:36 +0200

    Load Projects to View

---
+++ vendor/LoadProjects.js
@@ -0,0 +1,25 @@
+﻿function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+ 
+ajax_get('Projects/GetProjects', function(data) {
+    document.getElementById("description").innerHTML = data["Description"];
+ 
+    var html = "<p>" + data["Description"] + "</p>";
+    document.getElementById("descr").innerHTML = html;
+});
